f606126fcaff938258e5edf864008249cf940743
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-01 20:49:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Youtube Dl bot 😉
+## Prerequisite
+    ffmpeg
+  
+    
+## install dependencies
+    pip3 install -r requirements.txt
+
+
+## Setup Bot
+    - Change configuration config.py  File
+    - install dependencies
+    - python3 -m bot
+    
+## Credits ❤️
+* [Me](https://telegram.dog/TR-TECH-GUIDE)
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/TR-TECH-GUIDE/YTDownloaderBot)
---
+++ plugins/start.py
@@ -0,0 +1,17 @@
+
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Updates Channel", url="https://t.me/SLBotsOfficial")
+      ],
+      [ 
+        InlineKeyboardButton(
+            "Support Group", url="https://t.me/trtechguide")]
+    ])
+    welcomed = f"<b> Hey {message.from_user.first_name} , \n\nI'm YouTube DL Bot. I can download video or audio f\n rom Youtube. \n\nMade by @SLBotsOfficial 🇱🇰/help for More info </b>"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagatio
