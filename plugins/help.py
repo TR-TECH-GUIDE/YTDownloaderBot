158fb67a030379e72caffe22c6d6128d68e1fe1e
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-01 21:29:58 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -7,8 +7,10 @@ async def start(client, message):
     JoinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Updates Channel", url="https://t.me/SLBotsOfficial")
       ],
-        [InlineKeyboardButton(
-            "Support Group", url="https://t.me/trtechguide")]
+      [ 
+        InlineKeyboardButton(
+            "Support Group", url="https://t.me/trtechguide")
+      ]
     ])  
     helptxt = f"<b> Just send a Youtube url to download it in video or audio format!</b>"
     await message.reply_text(helptxt, reply_markup=JoinButton)
