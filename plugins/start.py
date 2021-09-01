dc2cb09e2f0ed61e7f884f790b3871498d66e137
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-01 21:21:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -5,12 +5,13 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    joinButton = InlineKeyboardMarkup([
+    InlineKeyboardButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Updates Channel", url="https://t.me/SLBotsOfficial")
       ],
       [ 
         InlineKeyboardButton(
-            "Support Group", url="https://t.me/trtechguide")]
+            "Support Group", url="https://t.me/trtechguide")
+      ]
     ])
     welcomed = f"<b> Hey {message.from_user.first_name} , \n\nI'm YouTube DL Bot. I can download video or audio f\n rom Youtube. \n\nMade by @SLBotsOfficial 🇱🇰/help for More info </b>"
     await message.reply_text(welcomed, reply_markup=joinButton)
