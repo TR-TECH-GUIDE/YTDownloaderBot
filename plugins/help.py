2cf11371292c2e4f999e21f396449c29b7a1df38
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-01 21:33:36 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, Filters,InlineKeyboardButton
+from pyrogram import Client, Filters,InlineKeyboardButton,InlineKeyboardMarkup
 
 
 @Client.on_message(Filters.command(["help"]))
